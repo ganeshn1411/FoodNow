c675d7b47fef447578dd71d95d225b71105787b7
Ganesh N <ganeshn1411>
2016-10-27 20:35:58 -0400

    first

---
+++ FN/app/src/main/java/com/example/ganesh/fn/Constants.java
@@ -0,0 +1,12 @@
+package com.example.ganesh.fn;
+
+/**
+ * Created by Ganesh on 10/27/2016.
+ */
+public class Constants {
+
+    public static final String FIRST_COLUMN="First";
+    public static final String SECOND_COLUMN="Second";
+    public static final String THIRD_COLUMN="Third";
+    public static final String FOURTH_COLUMN="Fourth";
+}
---
+++ FN/app/src/main/java/com/example/ganesh/fn/MyAdapter.java
@@ -0,0 +1,7 @@
+package com.example.ganesh.fn;
+
+/**
+ * Created by Ganesh on 10/27/2016.
+ */
+public class MyAdapter {
+}
